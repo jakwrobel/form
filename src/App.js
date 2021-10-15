5590e523b83c4cca2ee22909b17cff747d7c4da0
DESKTOP-AEJHR6R\Lenovo <kubus_96-1996@tlen.pl>
2021-10-15 19:59:50 +0200

    I add styles and change a bit jsx structure because of that.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import MyForm from './components/myform'
+import Greetings from './components/greetings'
+import './styles/theme/theme.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="page__wrap">
+      <Greetings/>
+      <MyForm/>
+      </div>
+ );
 }
 
 export default App;
